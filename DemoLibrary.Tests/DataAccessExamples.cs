2804cbe4a466bfde80d2777d35ef2de524d0bfab
Andrei <andrei_xavier@outlook.com>
2022-09-06 15:47:13 -0300

    novos testes adicionados

---
+++ DemoLibrary.Tests/DataAccessExamples.cs
@@ -0,0 +1,36 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class DataAccessExamples
+    {
+        [Fact]
+        public void AddPersonToPeopleList_ShouldWork()
+        {
+            PersonModel person = new PersonModel { FirstName = "Foo", LastName = "Bar" };
+            List<PersonModel> people = new List<PersonModel>();
+
+            DataAccess.AddPersonToPeopleList(people, person);
+
+            Assert.True(people.Count == 1);
+            Assert.Contains<PersonModel>(person, people);
+        }
+
+        [Theory]
+        [InlineData("Carl", "", "LastName")]
+        [InlineData("", "Neil", "FirstName")]
+        public void AddPersonToPeopleList_ShouldInvalid(string firstName, string lastName, string param)
+        {
+            PersonModel person = new PersonModel { FirstName = firstName, LastName = lastName };
+            List<PersonModel> people = new List<PersonModel>();
+
+            Assert.Throws<ArgumentException>(param, () => DataAccess.AddPersonToPeopleList(people, person));
+        }
+    }
+}
---
+++ DemoLibrary.Tests/ExamplesTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.IO;
+
+namespace DemoLibrary.Tests
+{
+    public class ExamplesTests
+    {
+        [Fact]
+        public void ExamplesLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = Examples.ExampleLoadTextFile("This is a valid file name.");
+
+            Assert.True(actual.Length > 0);
+        }
+
+        [Fact]
+        public void ExamplesLoadTextFile_InvalidNameShoudlFail()
+        {
+            Assert.Throws<ArgumentException>("file", () => Examples.ExampleLoadTextFile(""));
+        }
+    }
+}
